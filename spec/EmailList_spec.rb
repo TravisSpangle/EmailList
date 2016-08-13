47cc491c20bda1808a01ea557b68ee5c8faf2e4a
Travis Spangle <travis.spangle@k2sports.com>
2016-08-13 10:17:09 -0700

    iniital tests

--- spec/EmailList_spec.rb
+++ spec/EmailList_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe EmailList do
+  let(:sample_emails) { %w(one@test.com two@test.com two@test.com three@test.com) }
+
   it 'has a version number' do
     expect(EmailList::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'accepts a list of emails as arrays and passes back an array' do
+    expect(EmailList.uniq(sample_emails).class).to eq(Array)
   end
+
+  # it 'provides a list of uniq emails in the original order it takes them.'
+  # it 'processes 100,000 emails, where 50% are duplicated, and unsorted in under 1 second.'
 end
