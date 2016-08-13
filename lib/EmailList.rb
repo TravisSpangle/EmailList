11a44725983c86ae4c5e61aabdf23baa7262951f
Travis Spangle <travis.spangle@k2sports.com>
2016-08-13 10:25:56 -0700

    Accepts list and returns uniq list in same order

--- lib/EmailList.rb
+++ lib/EmailList.rb
@@ -2,6 +2,16 @@ require "EmailList/version"
 
 module EmailList
   def self.uniq(emails)
-    emails
+    registry    = {}
+    uniq_emails = []
+
+    emails.each do |email|
+      if registry[email].nil?
+        registry[email] = true
+        uniq_emails << email
+      end
+    end
+
+    uniq_emails
   end
 end
