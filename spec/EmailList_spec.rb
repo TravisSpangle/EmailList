782774ddfa7c04b253d165f7bc639073b7d15f2b
Travis Spangle <travis.spangle@k2sports.com>
2016-08-13 10:50:15 -0700

    test to ensure it passes in a timely manner

--- spec/EmailList_spec.rb
+++ spec/EmailList_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe EmailList do
   let(:sample_emails) { %w(one@test.com two@test.com two@test.com three@test.com) }
 
+  def timed(cleaning_emails)
+    time_start = Time.now
+    cleaning_emails.call
+    time_end = Time.now
+
+    time_end - time_start
+  end
+
   it 'has a version number' do
     expect(EmailList::VERSION).not_to be nil
   end
@@ -15,5 +23,20 @@ describe EmailList do
     expect(EmailList.uniq(sample_emails)).to eq(%w(one@test.com two@test.com three@test.com))
   end
 
-  # it 'processes 100,000 emails, where 50% are duplicated, and unsorted in under 1 second.'
+  it 'processes 100,000 emails under 1 second.' do
+    amount = 100_000
+    emails = []
+    (amount / 2).times do |n|
+      emails << "test_#{n}_@email_list.com"
+    end
+
+    # size doesn't matter
+    expect(EmailList.uniq(emails)).to eq(emails)
+
+    dirty_emails = proc do
+      EmailList.uniq((emails + emails).shuffle)
+    end
+
+    expect(timed(dirty_emails)).to be < 1
+  end
 end
